0cc9d7384f55814eda309300233e0513c6173948
measylite <support@brooklyncorners.com>
2016-08-04 23:38:40 -0700

    Updates made in one browser will be reflect in the other

--- HTML5_LocalStorage/app.js
+++ HTML5_LocalStorage/app.js
@@ -10,12 +10,17 @@ var saveBtn = document.getElementById('save');
 var retrieveBtn = document.getElementById('retrieve');
 var reviewBtn = document.getElementById('review');
 var deleteBtn = document.getElementById('delete');
+var clearBtn = document.getElementById('clear');
 
 // add event listner for each btn
 saveBtn.addEventListener('click', saveItem);
 retrieveBtn.addEventListener('click', retrieveItem);
 reviewBtn.addEventListener('click', reviewItem);
 deleteBtn.addEventListener('click', deleteItem);
+clearBtn.addEventListener('click',clearAll);
+
+// event listner to update the page of and changes to on other windows
+addEventListener('storage', reviewItem);
 
 }
 
@@ -23,33 +28,40 @@ function saveItem() {
 	// get key val
 	var key = document.getElementById('key').value;
 	var value = document.getElementById('value').value;
-	sessionStorage[key] = value;
+	localStorage[key] = value;
 }
 
 function retrieveItem() {
 	var data = document.getElementById('data');
 	var key = document.getElementById('key').value;
-	var value = sessionStorage[key];
+	var value = localStorage[key];
 
 	data.innerHTML = '<div>' + key + ': ' + value + '</div>';
 }
 function deleteItem() {
 	if (confirm('Delete ?')) {
 		var key = document.getElementById('key').value;
-		sessionStorage.removeItem(key);
+		localStorage.removeItem(key);
 		// msg
 		data.innerHTML = '<div>Item has been deleted!</div>';
 	};
 }
 
 function reviewItem() {
-	for (var i = 0; i < sessionStorage.length; i++) {
-		var key = sessionStorage.key(i);
-		var value = sessionStorage.getItem(key);
+	for (var i = 0; i < localStorage.length; i++) {
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);
 		// console.log(key);
-		// console.log(sessionStorage(key));
+		// console.log(localStorage(key));
 
 		data.innerHTML += '<div>' + key + ': ' + value + '<br></div>';
+	};
+}
+
+function clearAll() {
+	if (confirm('Do you want to clearAll?')) {
+		localStorage.clear();
+		data.innerHTML = '<div>Cleared All</div>';
 	};
 }
 addEventListener("load", measyInit);
