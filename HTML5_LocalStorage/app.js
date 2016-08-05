4144a83e126147cd56e427bc479b19a6bfa52460
measylite <support@brooklyncorners.com>
2016-08-04 22:46:29 -0700

    Example of sessionStorage

--- HTML5_LocalStorage/app.js
+++ HTML5_LocalStorage/app.js
@@ -1,5 +1,5 @@
 /*
- * We can use localStorage or localStorage to hold our incoming data
+ * We can use sessionStorage or localStorage to hold our incoming data
  * Do a search and replace to change from local or session
  * measyLite 
 */
@@ -23,31 +23,31 @@ function saveItem() {
 	// get key val
 	var key = document.getElementById('key').value;
 	var value = document.getElementById('value').value;
-	localStorage[key] = value;
+	sessionStorage[key] = value;
 }
 
 function retrieveItem() {
 	var data = document.getElementById('data');
 	var key = document.getElementById('key').value;
-	var value = localStorage[key];
+	var value = sessionStorage[key];
 
 	data.innerHTML = '<div>' + key + ': ' + value + '</div>';
 }
 function deleteItem() {
 	if (confirm('Delete ?')) {
 		var key = document.getElementById('key').value;
-		localStorage.removeItem(key);
+		sessionStorage.removeItem(key);
 		// msg
 		data.innerHTML = '<div>Item has been deleted!</div>';
 	};
 }
 
 function reviewItem() {
-	for (var i = 0; i < localStorage.length; i++) {
-		var key = localStorage.key(i);
-		var value = localStorage.getItem(key);
+	for (var i = 0; i < sessionStorage.length; i++) {
+		var key = sessionStorage.key(i);
+		var value = sessionStorage.getItem(key);
 		// console.log(key);
-		// console.log(localStorage(key));
+		// console.log(sessionStorage(key));
 
 		data.innerHTML += '<div>' + key + ': ' + value + '<br></div>';
 	};
