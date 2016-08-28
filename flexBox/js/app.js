8521efb909dbfbce1732850b76f3418260240b6c
measylite <support@brooklyncorners.com>
2016-08-28 02:40:18 -0700

    Demo Flex box with menu

---
+++ flexBox/js/app.js
@@ -0,0 +1,11 @@
+/*
+ * Demo flexBox
+ *  measylite
+*/
+$(document).foundation()
+
+function measyInit() 
+{
+	console.log('Welcome, measyInit() has been called on page load!');
+}
+addEventListener("load", measyInit);
